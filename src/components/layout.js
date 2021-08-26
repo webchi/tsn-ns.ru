b83af99a4dde532f7b011253b702f750151899ce
Vasiliy Shakhunov <v.shakhunov@gmail.com>
2021-08-26 21:12:46 +0300

    Add layout behaviour

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,17 +3,14 @@ import React from 'react';
 import 'bulma/bulma.sass';
 import './style_documentation.css'
 import Navbar from './navbar';
-import Header from './header'
 import Helmet from './helmet';
-import Midsection from './midsection';
 import Footer from './footer';
 
 const Layout = ({ children }) => (
 	<div>
 		<Helmet />
-		<Navbar />
-		<Header />		
-		<Midsection />
+		<Navbar />	
+		{children}
 		<Footer />
 	</div>
 );
